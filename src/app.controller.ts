958b40855a6518d7c86c015387a1b051e54093c6
edgex <edgexie@qq.com>
2023-08-14 09:43:00 +0800

    feat: add url-param query form-urlencoded form-data json

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Query,
+  Post,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
+import { AnyFilesInterceptor } from '@nestjs/platform-express';
 import { AppService } from './app.service';
-
+class PersonInfoDto {
+  name: string;
+  age: number;
+}
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -8,5 +21,34 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get(':id')
+  getHello2(@Param('id') id: string): string {
+    return this.appService.getHello(id);
+  }
+
+  @Get('find')
+  query(@Query('name') name: string) {
+    return 'your name is' + name;
+  }
+
+  @Post()
+  body(@Body() personInfoDto: PersonInfoDto) {
+    return 'your info is ' + JSON.stringify(personInfoDto);
+  }
+
+  @Post('file')
+  @UseInterceptors(
+    AnyFilesInterceptor({
+      dest: 'uploads/',
+    }),
+  )
+  body2(
+    @Body() personInfoDto: PersonInfoDto,
+    // @UploadedFiles() files: Array<Express.Multer.File>,
+  ) {
+    // console.log(files);
+    return `received: ${JSON.stringify(personInfoDto)}`;
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World22222242!';
+  getHello<T>(id?: T): string {
+    if (id) {
+      return 'H1' + id;
+    }
+    return 'HI';
   }
 }
